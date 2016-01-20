eecb3ccd6a8ec28c779044fe160a3cfa5831436f
Campbell <campbell@bikalabs.com>
2016-01-20 14:49:49 +0200

    Remove remaining sampletype=other from analysisrequest add form
    There was JS included for this which is no longer required

--- bika/bbk/static/js/bika.bbk.analysisrequest.add.js
+++ bika/bbk/static/js/bika.bbk.analysisrequest.add.js
@@ -7,14 +7,6 @@ function CustomAnalysisRequestAddView() {
 
     this.load = function() {
 
-        // https://jira.bikalabs.com/browse/WINE-66
-        // I will insert 'SampleType_hidden' key into all state variables.
-        // This allows me to include a default SampleType value in all ar columns,
-        // without requiring the rest of the column to be filled out.
-        var keys = Object.keys(window.bika.lims.ar_add.state);
-        for(var arnum=0; arnum < keys.length; arnum++){
-            window.bika.lims.ar_add.state[arnum]['SampleType_hidden'] = 1;
-        }
     };
 
 }
