e813d6e91ea43742e0b2d99944e0103f52c9351a
Campbell <campbell@bikalabs.com>
2016-01-21 11:23:36 +0200

    Hide some unused fields from ars and re-order remaining fields

--- bika/bbk/extenders/analysisrequest.py
+++ bika/bbk/extenders/analysisrequest.py
@@ -35,9 +35,27 @@ class AnalysisRequestSchemaModifier(object):
         self.context = context
 
     def fiddle(self, schema):
-        schema['ReportDryMatter'].widget.visible = False
+        schema['Batch'].widget.visible = False
+        schema['SubGroup'].widget.visible = False
+        schema['SamplingRound'].widget.visible = False
+        schema['Template'].widget.visible = False
+        schema['Specification'].widget.visible = False
+        schema['SamplePoint'].widget.visible = False
+        schema['StorageLocation'].widget.visible = False
+        schema['ClientOrderNumber'].widget.visible = False
         schema['SamplingDeviation'].widget.visible = False
+        schema['SamplingDeviation'].widget.visible = False
+        schema['SampleCondition'].widget.visible = False
+        schema['EnvironmentalConditions'].widget.visible = False
         schema['DefaultContainerType'].widget.visible = False
-        schema['AdHoc'].widget.visible = False
         schema['Composite'].widget.visible = False
+        schema['ReportDryMatter'].widget.visible = False
+        schema['InvoiceExclude'].widget.visible = False
+        schema['AdHoc'].widget.visible = False
+        schema['Priority'].widget.visible = False
+        schema['Sampler'].widget.visible = False
+
+        schema.moveField('Tank', before='Sample')
+        schema.moveField('Vintage', before='Tank')
+        schema.moveField('Cultivar', before='Vintage')
         return schema
